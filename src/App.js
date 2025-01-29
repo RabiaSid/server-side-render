91a36437b08114ae135bcb583791b2ce29e94eca
RabiaSid <rabiasid984@gmail.com>
2025-01-29 16:36:38 +0500

    bootstrap link work perfectly

--- src/App.js
+++ src/App.js
@@ -5,11 +5,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppLayout>
-        <AppRoute />
-      </AppLayout>
-    </div>
+    <>
+      <html>
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="description"
+            content="Web site created using create-react-app"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="stylesheet" href="./App.css"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Rubik&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+            crossorigin="anonymous"
+          />
+
+          <title>React App</title>
+        </head>
+        <body>
+          <div className="App">
+            <AppLayout>
+              <AppRoute />
+            </AppLayout>
+          </div>
+        </body>
+      </html>
+      <script src="/main.js" async=""></script>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
